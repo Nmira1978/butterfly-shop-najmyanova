ddbd0b54f1e22d19e3464c2df1f71f3f02dafee9
Mira Naymyanova <mira71615@gmail.com>
2022-01-25 20:54:56 +0300

    делала сайт, меню, табы для стр контакты

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,5 +1,53 @@
 $(document).ready(function(){
 
+  let isOpen = false;
+
+   $('.j-burger').on('click', function(){
+
+    $('.j-menu').show();
+
+    // if (isOpen) {
+    //   $('.j-menu').slideUp();
+    //   isOpen = false;
+    // } else {
+    //   $('.j-menu').slideDown();
+    //   isOpen = true;
+    // }
+
+    $('.j-menu').slideToggle();
+
+  });
+
+  // Табы на странице контактов
+
+  $('.j-tabs-link').on('click', function(event) {
+    event.preventDefault();
+
+    const index = $(this).index('.j-tabs-link');
+
+    $('.j-tabs-link').removeClass('active');
+    $(this).addClass('active');
+
+    $('.j-tabs-content').removeClass('active');
+    $('.j-tabs-content').eq(index).addClass('active');
+
+  });
+
+  // Фильтры
+
+    $('.j-filter-link').on('click', function(event) {
+      event.preventDefault();
+
+      const filter = $(this).data ('filter');
+      console.log(filter);
+
+      $('.j-filter-link').removeClass('active');
+      $(this).addClass('active');
+
+    });
+
+
+
 
 
 });
